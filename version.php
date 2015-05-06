47b1a33f0a4d7bcca56cbd819d94c65e5f90bf82
hansnok <hansjeria@gmail.com>
2015-05-06 16:16:39 -0400

    Initial commit

---
+++ version.php
@@ -0,0 +1,5 @@
+<?php
+    $plugin->version = 2015050601;
+    $plugin->requires = 2015040100;
+    $plugin->component = 'blocks_actividad_social'; // To check on upgrade, that module sits in correct place
+    
