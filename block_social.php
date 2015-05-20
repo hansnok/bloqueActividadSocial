ae7d48a300c76eb11f9d566b1d95272516180849
Hans Cristian Jeria Díaz <hansjeria@gmail.com>
2015-05-20 19:48:27 -0300

    Se agregan tablas display de assing y quiz en bloque

--- block_social.php
+++ block_social.php
@@ -26,10 +26,70 @@ class block_social extends block_base {
 			return false;
 		else{
 			$this->content = new stdClass;
-			$redirecturl = new moodle_url('local/actividadSocial/index.php', array('action'=>'agregar'));
-			//$this->content->text = $this->config->text;
-			$this->content->text = $OUTPUT->single_button($redirecturl,"Ver más");
-			$this->content->footer = '';
+			
+			$params = array(1,1,$course->id);
+			
+			//Traer las tareas
+			$sql_assing = "SELECT asub.id, a.name, us.firstname, us.lastname, asub.timecreated, asub.timemodified
+						 	FROM {course_modules} as cm INNER JOIN {modules} as m ON (cm.module = m.id) 
+						   		INNER JOIN {assign} as a ON (a.course = cm.course) 
+    					   		INNER JOIN {assign_submission} as asub ON ( asub.assignment = a.id) 
+    							INNER JOIN {user} as us ON (us.id = asub.userid) 
+						 	WHERE m.name in ('assign') 
+								AND cm.visible = ? 
+    							AND m.visible = ?
+    							AND cm.course = ?
+							ORDER BY asub.timemodified DESC,asub.id
+    					  	LIMIT 5";
+			$lastassings = $DB->get_records_sql($sql_assing, $params);
+			
+			//Traer los quiz
+			$sql_quiz = "SELECT qatt.id, q.name, us.firstname, us.lastname, qatt.timestart, qatt.timefinish
+						 	FROM {course_modules} as cm INNER JOIN {modules} as m ON (cm.module = m.id) 
+						   		INNER JOIN {quiz} as q ON (q.course = cm.course) 
+    					   		INNER JOIN {quiz_attempts} as qatt ON ( qatt.quiz = q.id) 
+    							INNER JOIN {user} as us ON (us.id = qatt.userid) 
+						 	WHERE m.name in ('quiz')
+								AND cm.visible = ? 
+    							AND m.visible = ?
+    							AND cm.course = ?
+    					  	ORDER BY qatt.timefinish DESC, qatt.id
+							LIMIT 5";
+			$lastquiz = $DB->get_records_sql($sql_quiz, $params);
+			
+			// Traer los recursos
+			$sql_resources = "SELECT *
+						 	FROM {course_modules} as cm INNER JOIN {modules} as m ON (cm.module = m.id) 
+						   		INNER JOIN {resource} as r ON (r.course = cm.course)
+						 	WHERE m.name in ('resource')
+								AND cm.visible = ? 
+    							AND m.visible = ?
+    							AND cm.course = ?
+    					  	ORDER BY m.name DESC LIMIT 5";
+			$lastresources = $DB->get_records_sql($sql_resources, $params);
+			
+			// Creación de tabla que muestra las últimas 5 tareas enviadas
+			$table_assign = new html_table();
+			$table_assign->head = array('Assing', '', '');
+			foreach($lastassings as $assing){
+				$timefinish = date('d-m-Y  H:i',$assing->timemodified);
+				$table_assign->data[] = array($assing->name,$assing->firstname." ".$assing->lastname, $timefinish);
+			}
+			
+			// Creación de tabla que muestra los ultimos 5 quiz
+			$table_quiz = new html_table();
+			$table_quiz->head = array('Quiz', '', '');
+			foreach($lastquiz as $quiz){
+				$timefinish = date('d-m-Y  H:i',$quiz->timefinish);
+				$table_quiz->data[] = array($quiz->name,$quiz->firstname." ".$quiz->lastname, $timefinish);
+			}
+			
+					
+			$lookassign = new moodle_url('local/actividadSocial/index.php', array('action'=>'assign'));
+			$lookquiz = new moodle_url('local/actividadSocial/index.php', array('action'=>'quiz'));
+			$this->content->text = html_writer::table($table_assign).$OUTPUT->single_button($lookassign,"See more").
+									"".html_writer::table($table_quiz).$OUTPUT->single_button($lookquiz,"See more");;
+			$this->content->footer = "";
 			
 			return $this->content;
 		}
@@ -37,12 +97,7 @@ class block_social extends block_base {
 
 	}
 
-	
-	function preferred_width() {
-		// El valor preferido está en pixeles
-		return 200;
-	}
-	
+
 	
 
 }
