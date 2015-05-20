c468f5a450bda0ee013621501b6e01cf83e9950a
Hans Cristian Jeria Díaz <hansjeria@gmail.com>
2015-05-19 21:40:15 -0300

    Bloque version alpha

---
+++ block_social.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Clase que define los links a funcionalidades propias del bloque actividad_social
+ * @author 2015 Hans Jeria (hansjeria@gmail.com)
+ *
+ */
+
+class block_social extends block_base {
+
+	// Inicializa el bloque
+	function init() {
+		$this->title = "Actividad Social";
+		$this->version = 2015050601;
+	}
+
+	// Función que genera el contenido del bloque
+	function get_content() {
+		if ($this->content !== NULL) {
+			return $this->content;
+		}
+	
+		$this->content = new stdClass;
+		$this->content->text = $this->config->text;
+		$this->content->footer = '';
+	
+		return $this->content;
+	}
+	
+	// Agrega icono para editar el bloque
+	function instance_allow_config() {
+		return true;
+	}
+	
+	// Carga la configuración al bloque
+	function specialization() {
+		$this->title = $this->config->title;
+	}
+	
+	// Posibilidad de configuración global
+	function has_config() {
+		return true;
+	}
+	
+	// Guarda la configuración
+	function config_save($data) {
+		// Comportamiento por defecto: graba todas las variables como propiedades $CFG
+		foreach ($data as $name => $value) {
+			set_config($name, $value);
+		}
+		return true;
+	}
+	
+	function instance_config_save($data) {
+		$data = stripslashes_recursive($data);
+		$this->config = $data;
+		return set_field('block_instance', 'configdata', base64_encode(serialize($data)),
+				'id', $this->instance->id);
+	}
+	
+
+	
+	function preferred_width() {
+		// El valor preferido está en pixeles
+		return 200;
+	}
+	
+	
+
+}
--- version.php
+++ version.php
@@ -23,6 +23,6 @@
 defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2015050601;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013110500;        // Requires this Moodle version
-$plugin->component = 'block_actividadsocial'; // Full name of the plugin (used for diagnostics)
+$plugin->component = 'block_social'; // Full name of the plugin (used for diagnostics)
 
   
