44658ec99cc78a916a183807bd6b1427e5f3f67d
Hans Cristian Jeria Díaz <hansjeria@gmail.com>
2015-05-20 15:05:23 -0300

    Bloque funciona hermoso

--- block_social.php
+++ block_social.php
@@ -15,51 +15,27 @@ class block_social extends block_base {
 
 	// Función que genera el contenido del bloque
 	function get_content() {
-		global $OUTPUT;
+		global $OUTPUT, $USER, $CFG, $DB, $PAGE, $COURSE;
+		
 		if ($this->content !== NULL) {
 			return $this->content;
 		}
-	
-		$this->content = new stdClass;
-		$redirecturl = new moodle_url('local/actividadSocial/index.php', array('action'=>'agregar'));
-		//$this->content->text = $this->config->text;
-		$this->content->text = $OUTPUT->single_button($redirecturl,"Ver más");
-		$this->content->footer = '';
-	
-		return $this->content;
-	}
-	
-	// Agrega icono para editar el bloque
-	function instance_allow_config() {
-		return true;
-	}
-	
-	// Carga la configuración al bloque
-	function specialization() {
-		$this->title = $this->config->title;
-	}
-	
-	// Posibilidad de configuración global
-	function has_config() {
-		return true;
-	}
-	
-	// Guarda la configuración
-	function config_save($data) {
-		// Comportamiento por defecto: graba todas las variables como propiedades $CFG
-		foreach ($data as $name => $value) {
-			set_config($name, $value);
+		
+		$course = $PAGE->course;
+		if(!$course || $course->id <= 1)
+			return false;
+		else{
+			$this->content = new stdClass;
+			$redirecturl = new moodle_url('local/actividadSocial/index.php', array('action'=>'agregar'));
+			//$this->content->text = $this->config->text;
+			$this->content->text = $OUTPUT->single_button($redirecturl,"Ver más");
+			$this->content->footer = '';
+			
+			return $this->content;
 		}
-		return true;
+		
+
 	}
-	
-	function instance_config_save($data) {
-		$data = stripslashes_recursive($data);
-		$this->config = $data;
-		return set_field('block_instance', 'configdata', base64_encode(serialize($data)),
-				'id', $this->instance->id);
-	}
-	
 
 	
 	function preferred_width() {
--- version.php
+++ version.php
@@ -20,6 +20,7 @@
  * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
 defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2015050601;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013110500;        // Requires this Moodle version
