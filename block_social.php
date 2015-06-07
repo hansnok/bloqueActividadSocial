7a1814431b1c7d7c7d075cf399aa5ceb5ab1cf73
Hans Cristian Jeria Díaz <hansjeria@gmail.com>
2015-06-07 01:37:31 -0300

    Agregados comentarios explicativos del codigo

--- block_social.php
+++ block_social.php
@@ -18,13 +18,14 @@
 /**
 * @package    local
 * @subpackage actividadSocial
-* @copyright  2015  Hans Jeria
+* @copyright  2015  Hans Jeria (hansjeria@gmail.com)
+* 			  2015  César Farías
 * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 */
 
 class block_social extends block_base {
 
-	// Inicializa el bloque
+	// Funcion que inicializa el bloque
 	function init() {
 		$this->title = get_string('socac','block_social');
 		$this->version = 2015060701;
@@ -32,21 +33,24 @@ class block_social extends block_base {
 
 	// Función que genera el contenido del bloque
 	function get_content() {
+		// variables globales de moodle
 		global $OUTPUT, $USER, $CFG, $DB, $PAGE, $COURSE;
 		
+		//verifica si el bloque esta cargado en el curso
 		if ($this->content !== NULL) {
 			return $this->content;
 		}
 		
+		// Condición que solo permite que el bloque sea visible dentro de un curso, de lo contrario no muestra nada
 		$course = $PAGE->course;
 		if(!$course || $course->id <= 1)
 			return false;
 		else{
+			// Cuando se esta dentro de un curso se carga todo el contenido del bloque
 			$this->content = new stdClass;
 			
-			$params = array(1,1,$course->id);
-			
-			//Traer las tareas
+			$params = array(1,1,$course->id);		
+			//Trae la información de las ultimas 5 tareas enviadas del curso
 			$sql_assing = "SELECT asub.id, a.name, us.firstname, us.lastname, asub.timecreated, asub.timemodified
 						 	FROM {course_modules} as cm INNER JOIN {modules} as m ON (cm.module = m.id) 
 						   		INNER JOIN {assign} as a ON (a.course = cm.course) 
@@ -58,9 +62,10 @@ class block_social extends block_base {
     							AND cm.course = ?
 							ORDER BY asub.timemodified DESC,asub.id
     					  	LIMIT 5";
+			// Consulta a la base de datos
 			$lastassings = $DB->get_records_sql($sql_assing, $params);
 			
-			//Traer los quiz
+			//Trae la informacion de los ultimos 5 quizes terminados
 			$sql_quiz = "SELECT qatt.id, q.name, us.firstname, us.lastname, qatt.timestart, qatt.timefinish
 						 	FROM {course_modules} as cm INNER JOIN {modules} as m ON (cm.module = m.id) 
 						   		INNER JOIN {quiz} as q ON (q.course = cm.course) 
@@ -72,9 +77,10 @@ class block_social extends block_base {
     							AND cm.course = ?
     					  	ORDER BY qatt.timefinish DESC, qatt.id
 							LIMIT 5";
+			// Consulta a la base de datos
 			$lastquiz = $DB->get_records_sql($sql_quiz, $params);
 			
-			// Traer los recursos
+			// Trae la informacion de los ultimos 5 recursos terminados
 			$sql_resources = "SELECT log.id, r.name, us.firstname, us.lastname, log.timecreated
 						 	FROM {course_modules} as cm INNER JOIN {modules} as m ON (cm.module = m.id) 
 						   		INNER JOIN {resource} as r ON (r.course = cm.course)
@@ -87,36 +93,45 @@ class block_social extends block_base {
     							AND cm.course = ?
     					  	ORDER BY log.timecreated DESC, log.id 
 							LIMIT 5";
+			// Consulta a la base de datos
 			$lastresources = $DB->get_records_sql($sql_resources, $params);
 			
-			// Creación de tabla que muestra las últimas 5 tareas enviadas
+			// Se crea la tabla que muestra la informacion sobre las ultimas tareas enviadas
 			$table_assign = new html_table();
+			// cabecera de la tabla
 			$table_assign->head = array(get_string('assign','block_social'));
+			// Se recore la informacion obtenida sobre las ultimas tareas enviadas para llenar la tabla
 			foreach($lastassings as $assing){
 				$timefinish = date('d-m-Y  H:i',$assing->timemodified);
 				$table_assign->data[] = array($assing->name,$assing->firstname." ".$assing->lastname, $timefinish);
 			}
 			
-			// Creación de tabla que muestra los ultimos 5 quiz
+			// Se crea la tabla que muestra la infomracion de los ultimos 5 quizes terminados
 			$table_quiz = new html_table();
+			// cabecera de la tabla
 			$table_quiz->head = array(get_string('quiz','block_social'));
+			// Se recore la informacion obtenida sobre lps ultimas quizes terminados para llenar la tabla
 			foreach($lastquiz as $quiz){
 				$timefinish = date('d-m-Y  H:i',$quiz->timefinish);
 				$table_quiz->data[] = array($quiz->name,$quiz->firstname." ".$quiz->lastname, $timefinish);
 			}
 			
-			// Creación de tabla que muestra los ultimos 5 archivos descargados
+			// Se crea la tabla que muestra la informacion de los ultimos 5 recursos descargados
 			$table_resource = new html_table();
+			// cabecera de la tabla
 			$table_resource->head = array(get_string('resources','block_social'));
+			// Se recore la informacion obtenida sobre los ultimas recursos descargados para llenar la tabla
 			foreach($lastresources as $resource){
 				$timefinish = date('d-m-Y  H:i',$resource->timecreated);
 				$table_resource->data[] = array($resource->name,$resource->firstname." ".$resource->lastname, $timefinish);
 			}
-					
+			
+			// Creacion de botones que redireccionan hacia el plugin en local
 			$lookassign = new moodle_url('../local/actividadsocial/index.php', array('action'=>'assign', 'cmid'=>$course->id));
 			$lookquiz = new moodle_url('../local/actividadsocial/index.php', array('action'=>'quiz', 'cmid'=>$course->id));
 			$lookresource = new moodle_url('../local/actividadsocial/index.php', array('action'=>'resource', 'cmid'=>$course->id));
 			
+			// Con las tablas y los botones listos, estos se concatenan y se asignan como contenido del bloque
 			$this->content->text = html_writer::table($table_assign).$OUTPUT->single_button($lookassign,get_string('seemore','block_social')).
 									html_writer::table($table_quiz).$OUTPUT->single_button($lookquiz,get_string('seemore','block_social')).
 									html_writer::table($table_resource).$OUTPUT->single_button($lookresource,get_string('seemore','block_social'));
